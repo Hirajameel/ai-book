556047a84ed96fc05f0a2a0330ce145f9762a1d3
Hira-Jameel <160866608+Hirajameel@users.noreply.github.com>
2025-12-24 00:18:23 +0500

    change

--- frontend-docu/docusaurus.config.js
+++ frontend-docu/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://Hirajameel.github.io',
+  url: 'https://hirajameel.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ai-book/',
